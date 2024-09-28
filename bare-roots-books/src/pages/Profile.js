7187f59e6e8c3252251e1c3e23291168f6128edd
Berucha Wilson-Velazquez <35301763+Berucha@users.noreply.github.com>
2024-09-28 01:32:02 +0000

    user profile placeholder

---
+++ bare-roots-books/src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'; // Imports React
+
+function Profile() {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>This is where user profile info will go, including their favorite books and personal info.</p>
+    </div>
+  );
+}
+
+export default Profile; // Exports the Profile page component
