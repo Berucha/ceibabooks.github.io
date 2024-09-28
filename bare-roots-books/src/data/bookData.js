e21a2301ce98a78ca79f69793ff602837ad8925c
Berucha Wilson-Velazquez <35301763+Berucha@users.noreply.github.com>
2024-09-28 01:24:36 +0000

    Adding book data and corresponding book cover images

---
+++ bare-roots-books/src/data/bookData.js
@@ -0,0 +1,72 @@
+// data/booksData.js
+// Defines an array of book objects with relevant metadata.
+const books = [
+    {
+        title: "African American Herbalism: A Practical Guide to Healing Plants and Folk Traditions",
+        author: "Lucretia VanDyke",
+        color: "lightblue",
+        cover: "/book-covers/African_American_Herbalism_Traditions.jpg",
+    },
+    {
+        title: "The Lost Apothecary",
+        author: "Sarah Penner",
+        color: "lightgreen",
+        cover: "/book-covers/The_Lost_Apothecary.jpg",
+    },
+    {
+        title: "The Apothecary",
+        author: "Maile Meloy",
+        color: "lightpink",
+        cover: "/book-covers/The_Apothecary.jpg",
+    },
+    {
+        title: "The Apprentices",
+        author: "Maile Meloy",
+        color: "lightpink",
+        cover: "/book-covers/The_Apprentices.jpg",
+    },
+    {
+        title: "The After-Room",
+        author: "Maile Meloy",
+        color: "lightpink",
+        cover: "/book-covers/The_After-Room.jpg",
+    },
+    {
+        title: "Mr. Penumbra’s 24-Hour Bookstore",
+        author: "Robin Sloan",
+        color: "lightpink",
+        cover: "/book-covers/Mr.Penumbra_24-Hour_Bookstore.jpg",
+    },
+    {
+        title: "Ajax Penumbra 1969",
+        author: "Robin Sloan",
+        color: "lightpink",
+        cover: "/book-covers/Ajax_Penumbra_1969.jpg",
+    },
+    {
+        title: "The Lost for Words Bookshop",
+        author: "Stephanie Butland",
+        color: "lightpink",
+        cover: "/book-covers/The_Lost_for_Words_Bookshop_A_Novel.jpg",
+    },
+    {
+        title: "The Midnight Library",
+        author: "Matt Haig",
+        color: "lightpink",
+        cover: "/book-covers/The_Midnight_Library.jpg",
+    },
+    {
+        title: "She Who Became the Sun",
+        author: "Shelley Parker-Chan",
+        color: "lightpink",
+        cover: "/book-covers/She_Who_Became_the_Sun.jpg",
+    },
+    {
+        title: "Lobizona",
+        author: "Romina Garber",
+        color: "lightpink",
+        cover: "/book-covers/Lobizona.jpg",
+    },
+];
+
+export default books;   // Exports the books data for use in the Bookshelf component.
