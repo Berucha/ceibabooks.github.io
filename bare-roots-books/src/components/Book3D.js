00c31855f5853895b3316a2fc36ce9a199a271d9
Berucha Wilson-Velazquez <35301763+Berucha@users.noreply.github.com>
2024-09-28 17:08:19 +0000

    Each book's color in bookData will become the spinecolor for the sides that don't display the book's cover image (once I figure that out - right now it's commented out because it makes the entire book the color) , also added some shading but need to figugre out how to make it not so shadowy

--- bare-roots-books/src/components/Book3D.js
+++ bare-roots-books/src/components/Book3D.js
@@ -2,13 +2,20 @@ import React from 'react'; // Imports React
 import { useTexture } from '@react-three/drei'; // useTexture for loading textures
 
 // Book3D component renders a 3D book with a texture on it
-function Book3D({ position, textureUrl }) {
-  const texture = useTexture(textureUrl); // Loads the book cover texture
+function Book3D({ position, coverTextureUrl, spineColor, width, height, depth }) {
+  const coverTexture = useTexture(coverTextureUrl); // Loads the book cover texture
 
   return (
-    <mesh position={position}>
-      <boxGeometry args={[1, 2, 0.5]} /> {/* 3D book shape (box) */}
-      <meshStandardMaterial map={texture} /> {/* Applies the book cover as texture */}
+    <mesh position={position} castShadow receiveShadow> {/* Allows the book to cast and receive shadows */}
+      <boxGeometry args={[width, height, depth]} /> {/* Dynamic 3D book shape */}
+
+      {/* Applies the book cover as texture for the front and applies different materials for the other sides */}
+      <meshStandardMaterial attachArray="material" map={coverTexture} /> {/* Front face (cover) */}
+      {/* <meshStandardMaterial attachArray="material" color={spineColor} /> Back face */}
+      {/* <meshStandardMaterial attachArray="material" color={spineColor} /> Top face */}
+      {/* <meshStandardMaterial attachArray="material" color={spineColor} /> Bottom face */}
+      {/* <meshStandardMaterial attachArray="material" color={spineColor} /> Left face (spine) */}
+      {/* <meshStandardMaterial attachArray="material" color={spineColor} /> Right face */}
     </mesh>
   );
 }
