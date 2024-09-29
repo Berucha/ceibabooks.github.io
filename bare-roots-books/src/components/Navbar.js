b5252ef8e72b0c68b59840485eb17d5e5ef42114
Berucha Wilson-Velazquez <35301763+Berucha@users.noreply.github.com>
2024-09-29 18:11:47 +0000

    Design updates and realized the styles weren't applying properly

--- bare-roots-books/src/components/Navbar.js
+++ bare-roots-books/src/components/Navbar.js
@@ -11,20 +11,20 @@ function NavigationBar() {
         {/* App name or logo */}
         <Navbar.Brand href="/" className="navbarBrand">Bare Roots Books</Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" /> {/* Toggle button for mobile view */}
+        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> // Toggle button for mobile view */}
         <Navbar.Collapse id="basic-navbar-nav"> {/* Collapsible menu for mobile view */}
           <Nav className="me-auto">
             {/* Links to different pages */}
-            <Nav.Link href="/" className="navbar-link">Home</Nav.Link>
-            <Nav.Link href="/bookshelf" className="navbar-link">Bookshelf</Nav.Link>
-            <Nav.Link href="/profile" className="navbar-link">Profile</Nav.Link>
+            <Nav.Link href="/" className="navbarLink">Home</Nav.Link>
+            <Nav.Link href="/bookshelf" className="navbarLink">Bookshelf</Nav.Link>
+            <Nav.Link href="/profile" className="navbarLink">Profile</Nav.Link>
 
             {/* Dropdown menu for more links */}
-            <NavDropdown title="More" id="basic-nav-dropdown">
+            {/* <NavDropdown title="More" id="basic-nav-dropdown">
               <NavDropdown.Item href="/about">About Us</NavDropdown.Item>
               <NavDropdown.Item href="/contact">Contact</NavDropdown.Item>
               <NavDropdown.Item href="/terms">Terms of Service</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
