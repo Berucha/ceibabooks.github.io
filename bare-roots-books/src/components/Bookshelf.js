104dbdc2a9ef07e81c8f1a3ce8c87c15ecd53630
Berucha Wilson-Velazquez <35301763+Berucha@users.noreply.github.com>
2024-09-29 22:17:53 +0000

    Design updates to bookshelf that: - introduces bookshelf decor - user customization of decor but I commented it out because didn't like how it looked and was having trouble with positioning it - also added progress bar to track reading progress

--- bare-roots-books/src/components/Bookshelf.js
+++ bare-roots-books/src/components/Bookshelf.js
@@ -1,28 +1,46 @@
 /* src/components/Bookshelf.js */
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/components/Bookshelf.css';
 
 const Bookshelf = () => {
+  const [decor, setDecor] = useState(['🌿', '🌱']);
+
   const books = [
-    { title: 'Book 1', author: 'Author 1' },
-    { title: 'Book 2', author: 'Author 2' },
-    { title: 'Book 3', author: 'Author 3' },
+    { title: 'Book 1', author: 'Author 1', progress: 70 }, // Added progress property
+    { title: 'Book 2', author: 'Author 2', progress: 50 }, // Added progress property
+    { title: 'Book 3', author: 'Author 3', progress: 30 }, // Added progress property
   ];
+
+  const handleDecorChange = () => {
+    const newDecor = prompt('Enter new decor items separated by commas (e.g., 🌿,🌱,📚):');
+    if (newDecor) {
+      setDecor(newDecor.split(','));
+    }
+  };
 
   return (
     <div className="bookshelf">
+      {/* <button className="decor-settings-button" onClick={handleDecorChange}>
+        Customize Decor
+      </button> */}
       <div className="shelf">
-        {books.map((book, index) => (
-          <div key={index} className="book">
-            <div className="book-cover">
-              <div className="book-title">{book.title}</div>
-              <div className="book-author">{book.author}</div>
+        <div className="decor plant">{decor[0]}</div>
+        <div className="books">
+          {books.map((book, index) => (
+            <div key={index} className="book">
+              <div className="progress-bar">
+                <div className="progress" style={{ width: `${book.progress}%` }}></div>
+              </div>
+              <div className="book-cover">
+                <div className="book-title">{book.title}</div>
+                <div className="book-author">{book.author}</div>
+              </div>
             </div>
-          </div>
-        ))}
-        <div className="decor plant">🌿</div>
-        <div className="decor plant">🌱</div>
+          ))}
+        </div>
+        <div className="decor plant">{decor[1]}</div>
       </div>
+      <div className="floor-decor">{decor[2]}</div>
     </div>
   );
 };
